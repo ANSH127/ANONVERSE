7b3d08a0254e0c58f3b1d5aa597ee4b62b8b10c7
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-02-03 11:09:18 +0530

    adding like feature

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,52 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image, Alert } from 'react-native'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ChatBubbleBottomCenterIcon, HeartIcon } from 'react-native-heroicons/outline'
 import { HeartIcon as HeartIcon2 } from 'react-native-heroicons/solid'
 import { formatDistance } from 'date-fns'
-
+import { confessionRef, auth } from '../config/firebase'
+import { updateDoc, doc } from 'firebase/firestore'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Card({ item }) {
     const [like, setLike] = React.useState(0);
+    const navigation = useNavigation()
+
+    const handleLike = async (val) => {
+
+        try {
+            if (val === 1 && !item.likedby.includes(auth.currentUser.uid)) {
+                const user = auth.currentUser;
+                await updateDoc(doc(confessionRef, item.id), {
+                    likes: item.likes + 1,
+                    likedby: [...item.likedby, user.uid]
+
+                })
+                item.likedby.push(user.uid)
+                item.likes = item.likes + 1
+                setLike(1)
+                Alert.alert("Liked")
+            }
+            else if(val === 0 && item.likedby.includes(auth.currentUser.uid)) {
+                const user = auth.currentUser;
+                await updateDoc(doc(confessionRef, item.id), {
+                    likes: Math.max(item.likes - 1, 0),
+                    likedby: item.likedby.filter((id) => id !== user.uid)
+
+                })
+
+                item.likedby = item.likedby.filter((id) => id !== auth.currentUser.uid)
+                item.likes = Math.max(item.likes - 1, 0)
+                setLike(0)
+                Alert.alert("Unliked")
+            }
+
+        } catch (error) {
+            Alert.alert(error.message)
+
+        }
+
+    }
 
     return (
         <View className=' p-4
@@ -36,14 +75,23 @@ export default function Card({ item }) {
                     <TouchableOpacity
 
                     >
-                        {like === 0 ? <HeartIcon size={30} color='#3B82F6' onPress={() => setLike(1)} /> : <HeartIcon2 size={30} color='#3B82F6' onPress={() => setLike(0)} />
-                        }
+                        {
+                            item.likedby.includes(auth.currentUser.uid) || like ?
+                             <HeartIcon2 size={30} color='#3B82F6'  onPress={() => handleLike(0)} 
+                             
+                              />
+                             : 
+                             <HeartIcon size={30} color='#3B82F6' 
+                             
+                              onPress={() => handleLike(1)} />}
+
+
 
                         {/* // like count */}
 
                     </TouchableOpacity>
                     <Text className='text-gray-500 font-semibold p-1'>
-                        {item.likes + like}
+                        {Math.max(item.likes, 0)}
                     </Text>
                 </View>
 
