b1ddccee855b165223ed95b0ff0ace870ec320c9
Ansh Agarwal <anshagrawal12348@gmail.com>
2024-02-03 09:18:36 +0530

    adding loader in profilescreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,13 @@
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity, Alert } from 'react-native'
 import React, { useEffect } from 'react'
 import ScreenWrapper from '../components/ScreenWrapper'
-import { auth,usersRef } from '../config/firebase'
-import { getDocs, or, query, where,orderBy } from 'firebase/firestore'
-
+import { auth, usersRef } from '../config/firebase'
+import { getDocs, or, query, where, orderBy } from 'firebase/firestore'
+import Loadar from '../components/Loadar'
 
 export default function ProfileScreen() {
+    const [loading, setLoading] = React.useState(false)
+
 
     const handleLogout = () => {
         auth.signOut()
@@ -14,17 +16,30 @@ export default function ProfileScreen() {
     const [userData, setUserData] = React.useState({})
 
     const fetchUserData = async () => {
-        const user = auth.currentUser
-        if (user) {
-            let data = []
-            const q = query(usersRef, where('uid', '==', user.uid))
-            const querySnapshot = await getDocs(q)
-            querySnapshot.forEach((doc) => {
-                    data.push({...doc.data(),id:doc.id})
+        try {
+            setLoading(true)
+            const user = auth.currentUser
 
-            });
-            setUserData(data[0])
+            if (user) {
+                let data = []
+                const q = query(usersRef, where('uid', '==', user.uid))
+                const querySnapshot = await getDocs(q)
+                querySnapshot.forEach((doc) => {
+                    data.push({ ...doc.data(), id: doc.id })
+
+                });
+                setUserData(data[0])
+            }
+
+        } catch (error) {
+            Alert.alert(error.message)
+
         }
+        finally {
+            setLoading(false)
+        }
+
+
     }
 
     useEffect(() => {
@@ -36,23 +51,26 @@ export default function ProfileScreen() {
             <View className='items-center'>
                 <Image className='rounded-full' source={require('../assets/images/Avatar.jpg')} style={{ width: 200, height: 200 }} />
                 {/* // welcome user */}
+
                 <Text className='text-3xl text-black font-semibold pt-2'>
-                     {userData.name}
+                    {userData.name}
                 </Text>
                 {/* // show user email */}
-                <View className='w-full justify-center p-4'>
-                    <Text className='text-xl text-black font-semibold '>Email:</Text>
+                {loading ? <Loadar /> :
+                    <>
+                        <View className='w-full justify-center p-4'>
+                            <Text className='text-xl text-black font-semibold '>Email:</Text>
 
 
-                    <TextInput className='p-4 mb-3 bg-blue-100 text-gray-700 rounded-2xl w-full' defaultValue={userData.email}   editable={false} selectTextOnFocus={false} />
-                </View>
-                {/* logout button */}
+                            <TextInput className='p-4 mb-3 bg-blue-100 text-gray-700 rounded-2xl w-full' defaultValue={userData.email} editable={false} selectTextOnFocus={false} />
+                        </View>
 
-                <View className='w-full justify-center p-4'>
-                    <TouchableOpacity className='bg-blue-200 p-3 rounded-2xl' onPress={handleLogout}  >
-                        <Text className='text-center text-xl text-gray-700 font-bold'>Logout</Text>
-                    </TouchableOpacity>
-                </View>
+                        <View className='w-full justify-center p-4'>
+                            <TouchableOpacity className='bg-blue-200 p-3 rounded-2xl' onPress={handleLogout}  >
+                                <Text className='text-center text-xl text-gray-700 font-bold'>Logout</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </>}
 
 
 
